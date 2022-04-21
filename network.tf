0b28902e4910bf8c45466464b266bed10cf56b23
Johann Strauss <jstrauss123@gmail.com>
2022-04-21 20:02:54 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,4 +2,5 @@ module "network" {
   source  = "app.terraform.io/starbucks-jsheaman/network/azurerm"
   version = "3.5.0"
   # insert required variables here
+  resource_group_name = "jstrauss-networkgn"
 }
